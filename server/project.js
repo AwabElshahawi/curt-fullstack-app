38678864f49dce35892c4127fe4f0451ab65e6ba
Awab Elshahawi <awabelshahawi@gmail.com>
2026-09-22 01:16:55 +0300

    1:15 Tue 22/09 :
    Finished defining and creating models/schemas for users, tasks and projects

---
+++ server/project.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim : true,
+    },
+    description: {
+        type: String,
+        required: false,
+        trim: true,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    members: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+},
+    {timestamps: true,}
+)
+
+const Project = mongoose.model('Project', projectSchema);
+module.exports = Project;
+
